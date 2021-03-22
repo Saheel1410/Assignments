fce68d34ecc2f8bc18b24bd08cdeac2846742121
Saheel1410 <80039754+Saheel1410@users.noreply.github.com>
2021-03-22 10:49:04 -0700

    java programs

---
+++ RemoveDuplicates.java
@@ -0,0 +1,22 @@
+package assihnments_java;
+
+public class RemoveDuplicates {
+
+	public static void main(String[] args) {
+		String str = "We learn java basics as part of java sessions in java week1";
+		
+		String[] eachWords = str.split("\\s+");
+		for (int i =0; i <eachWords.length; i++) {
+			
+			for (int j = i+1; j <eachWords.length; j++) {
+		
+				if((eachWords[i].equals(eachWords[j]))){
+
+					eachWords[j]="";			}
+			
+		}
+			System.out.print(eachWords[i]+" ");	
+	}
+
+}
+}
